261894e239d06804457082500b878ee67fd208fc
Shazia Bee <shaziabee9027@gmail.com>
2024-07-05 15:39:15 +0530

    Add files via upload

---
+++ fx-portal-main/hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  networks: {
+    mumbai: {
+      url: 'https://rpc-mumbai.maticvigil.com',
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    sepolia: {
+      url: 'https://rpc.sepolia.org',
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  }
+};
